3029e5f58e78ae14dbc09a8c1956c82aac670833
SAFNAS <iamsafnas@gmail.com>
2020-06-04 18:00:01 +0530

    Updates

---
+++ precache-manifest.32f6a3c759aa3c0266677f8b7d9d7109.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5864dac7bf7b5797695a21dd7756a128",
+    "url": "/react-basics-tutorial-tania/index.html"
+  },
+  {
+    "revision": "f1a02ee17c84c6885394",
+    "url": "/react-basics-tutorial-tania/static/css/main.9447384d.chunk.css"
+  },
+  {
+    "revision": "1a6d1598dadeba2b2ece",
+    "url": "/react-basics-tutorial-tania/static/js/2.721aeaa8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-basics-tutorial-tania/static/js/2.721aeaa8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f1a02ee17c84c6885394",
+    "url": "/react-basics-tutorial-tania/static/js/main.3bbfe36d.chunk.js"
+  },
+  {
+    "revision": "fc314208f569b58734e5",
+    "url": "/react-basics-tutorial-tania/static/js/runtime-main.087a76a3.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-tutorial/precache-manifest.ce71d0df3f4c22a811bfa3517e03fbf5.js"
+  "/react-basics-tutorial-tania/precache-manifest.32f6a3c759aa3c0266677f8b7d9d7109.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-tutorial/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-basics-tutorial-tania/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
