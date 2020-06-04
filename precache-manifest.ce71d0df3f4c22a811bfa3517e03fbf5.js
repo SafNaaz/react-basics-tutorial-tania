58c98e206d35c8237699c98ef83fa5e9a473e66b
SAFNAS <iamsafnas@gmail.com>
2020-06-04 17:51:52 +0530

    Updates

---
+++ precache-manifest.ce71d0df3f4c22a811bfa3517e03fbf5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c019a20f12d7e9b7f1fab88c6e657a1",
+    "url": "/react-tutorial/index.html"
+  },
+  {
+    "revision": "f1a02ee17c84c6885394",
+    "url": "/react-tutorial/static/css/main.9447384d.chunk.css"
+  },
+  {
+    "revision": "1a6d1598dadeba2b2ece",
+    "url": "/react-tutorial/static/js/2.721aeaa8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-tutorial/static/js/2.721aeaa8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f1a02ee17c84c6885394",
+    "url": "/react-tutorial/static/js/main.3bbfe36d.chunk.js"
+  },
+  {
+    "revision": "e2d7e324c695c2bd8632",
+    "url": "/react-tutorial/static/js/runtime-main.3c738b8a.js"
+  }
+]);
